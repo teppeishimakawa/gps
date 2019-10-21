f0af054eff5c082de182090608682399a2648987
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-21 16:13:36 +0900

    11

--- gps.js
+++ gps.js
@@ -219,7 +219,7 @@ if((dis1 < 20000) && (localStorage.getItem("lot1") != 1))
   document.getElementById("lot1").disabled="";
   }else
   {
-    //document.getElementById("lot1").disabled="true";
+    document.getElementById("lot1").disabled="true";
     marker_p1.setAnimation(null);
   }
 
@@ -236,7 +236,7 @@ if((dis2 < 50000) && (localStorage.getItem("lot2") != 1))
   document.getElementById("lot2").disabled="";
   }else
   {
-    //document.getElementById("lot2").disabled="true";
+    document.getElementById("lot2").disabled="true";
     marker_p2.setAnimation(null);
   }
 
@@ -252,7 +252,7 @@ if((dis3 < 50000) && (localStorage.getItem("lot3") != 1))
   document.getElementById("lot3").disabled="";
   }else
   {
-    //document.getElementById("lot3").disabled="true";
+    document.getElementById("lot3").disabled="true";
     marker_p3.setAnimation(null);
   }
 
