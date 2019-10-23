cd30e1d607cdcf95f1c221252d63d68d55db73cf
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-23 10:07:36 +0900

    949

--- gps.js
+++ gps.js
@@ -61,17 +61,77 @@ if(!map)
      center: {lat:position.coords.latitude , lng:position.coords.longitude},
      zoom: 15,
      styles:
-     [
-       //全てのラベルを非表示
-       {
-        featureType: 'all',
-        elementType: 'labels',
-        stylers:
-        [
-          {visibility: 'on'},
-        ],
-       },
-     ]
+[
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#e0efef"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "hue": "#1900ff"
+            },
+            {
+                "color": "#c0e8e8"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 700
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#7dcdcd"
+            }
+        ]
+    }
+]
     });
  }
 
